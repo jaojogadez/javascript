51c2a83c4993f715d2dbd6ad62117c0cd92cf6cc
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-12 10:12:49 -0300

    comentários em js

--- Rocktseat/aula001 - Connect JS with HTML/script.js
+++ Rocktseat/aula001 - Connect JS with HTML/script.js
@@ -1,3 +1,17 @@
 console.log('Hello Word!!!');
+// Comentário in-line em JS
 console.log('Mensagem do JS com o HTML');
 console.error('João Pedro D. Oliveira')
+
+// Comentários podem ser usados para explicar partes do código e funções, bom para quem é iniciante
+
+
+/*
+    Comentário de múltiplas linhas 
+    Isso não afetará o código principal lido pelo navegador
+
+    Pq usar comentários nos estudos:
+    - Fornecer lembretes de coisas a fazer 
+    - Documentar o código
+    - Relemnbrar no futuro oq faz
+*/
