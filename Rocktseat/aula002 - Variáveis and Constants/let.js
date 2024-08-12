62cf7aefcb54b8579ce471fa42d099eb4bc9e680
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-12 10:45:13 -0300

    update

---
+++ Rocktseat/aula002 - Variáveis and Constants/let.js
@@ -0,0 +1,10 @@
+let user2 
+console.log("João Pedro")
+
+// O let não permite criar variáveis com o mesmo nome, pórem vc pode substituir o contéudo
+
+let email2 = "neto@email.com"
+console.log(email2)
+
+email2 = "marcos@email.com"
+console.log(email2)
---
+++ Rocktseat/aula002 - Variáveis and Constants/var.js
@@ -0,0 +1,34 @@
+/*Na programação VAR é um espaco reservado na memória RAM do  pc para armazenar algo temporariamente.
+
+ Dar nomes as VAR é super importante. - Facilita a Identificação.
+
+ O Conteúdo de uma VAR pode variar, ou seja, pode ser alterado.
+*/
+
+// Declara uma variável sem valor
+var user
+console.log(user)
+
+// Declara uma variável com valor
+var email = "joaopedrod.oliveria@email.com"
+console.log(email)
+
+// Substitui o valor da var
+email = "thiago@email"
+console.log(email)
+
+
+/*
+  Quando uma linguagem de programação é case-sensitive siginifica que ela é sensível a letras maiúsculas e minúsculas.
+*/
+
+var product = "Tecla de Mecânico"
+var Product = "Mouse Gamer"
+var product = "Fone sem fio" // Valor da var foi alterado
+
+console.log(product)
+console.log(Product)
+
+/*
+  O JS é case-sensitive
+*/
