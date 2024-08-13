d70bd8a1c84697a6217afbe00a2cbfb32c9a8bbc
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-13 11:57:30 -0300

    mais uma aula

---
+++ Rocktseat/aula002 - Variáveis and Constants/scope/script.js
@@ -0,0 +1,27 @@
+// console.log(user)
+// var user = "João"
+
+// //Hosting 
+
+// // var user
+// // console.log(user)
+
+var email = 'joão@email.com' //global scope
+{
+    //block scope
+    console.log(email)
+}
+
+
+{
+    var age = 18
+}
+
+console.log(age)
+
+
+let adress = "Rua X"
+{
+    console.log(adress)
+}
+
