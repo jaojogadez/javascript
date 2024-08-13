dd0b9442cfd21e1a60128ccd8dd5946e35f9f763
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-13 12:12:06 -0300

    mais uma aula

---
+++ Rocktseat/aula002 - Variáveis and Constants/nome/js.js
@@ -0,0 +1,40 @@
+// case - sensitive
+let username = 'Rodrigo'
+let userName = 'João'
+
+console.log(username)
+console.log(userName)
+
+// Podemos
+
+let $email = "marcos@emai.com"
+let _email = "neto@emai.com"
+let user_emai = "cristina@emai.com"
+let Ação = "Cadastrar" // porém não recomendado
+
+console.log($email)
+console.log(_email)
+console.log(user_emai)
+console.log(Ação)
+
+// Não podemos
+// começar com número
+// let 1nome = .....
+// usar espaço na var
+
+// recomendações
+/* 
+    1 escrever em inglês
+    2 não usar caracteres especiais
+    3 usar nomes especificos, não 'x' por exemplo
+*/
+
+// RECOMENDAÇÃO camelCase(corcunda do camelo) :/ sério?
+let productName = 'shampoo'
+let firstName = "João"
+let lastName = 'Oliveira'
+
+// RECOMENDAÇÃO snake_case
+let product_name = 'Bola'
+let first_name = 'Neto'
+let last_name = 'Paulo'
