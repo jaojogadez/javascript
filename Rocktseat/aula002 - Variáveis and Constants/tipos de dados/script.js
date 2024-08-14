df5057dbfc7f5a33b32d7fcb39ac26c9dbaad0e7
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-14 11:57:17 -0300

    mais uma aula do rocket seat

---
+++ Rocktseat/aula002 - Variáveis and Constants/tipos de dados/script.js
@@ -0,0 +1,13 @@
+//String
+let userName = "Heloise Karen"
+console.log(userName)
+console.log(typeof userName)
+
+console.log("String com aspas duplas")
+console.log('String apóstrofo')
+
+// Quando utilizar uma ou outra?
+
+//Basicamente se vc quiser usar aspas dentro de uma string
+console.log("Uma string com 'apóstrofo' dentro ")
+console.log('Uma string com "aspas duplas" dentro ')
