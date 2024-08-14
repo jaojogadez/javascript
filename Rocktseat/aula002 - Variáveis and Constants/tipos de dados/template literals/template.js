a03d6384cd75ce22b69056818a5f1da6f5d07655
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-14 12:03:58 -0300

    mais uma aula

---
+++ Rocktseat/aula002 - Variáveis and Constants/tipos de dados/template literals/template.js
@@ -0,0 +1,10 @@
+
+// interpolação de strings
+
+let usarname  = "João"
+let email = "joão@email.com"
+
+console.log(usarname, email, "msg")
+
+
+// Template Literals é o placeholder q o sor ensinou lá na escola com `` 
