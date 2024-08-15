5fe96b7454acd1bc5e25be4f8d4e958812afff72
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-15 11:50:42 -0300

    + 2 aulas

---
+++ Rocktseat/aula 012 - Arithmetic operadors/script.js
@@ -0,0 +1,14 @@
+// Operadores Aritméticos
+
+console.log("Soma: ", 12 + 8.6) // coloque ponto pra dizer vírgula
+console.log("Concatenação: ", "12" + "8") // União de texto !cuidado
+
+console.log("Subtração: ", 12 -17)
+
+console.log("Multiplicação: ", 12 * 17 - 22)
+
+console.log("Divisão: ", (22+17)/12)
+
+console.log("Resto de Divisão ", (22 + 17) % 12)
+
+console.log("Exponencial: ", 2**4) // faz potência 
