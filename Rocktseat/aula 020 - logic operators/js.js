63f8a9b5e012cac726269fce63c04a77c49eaa2b
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-16 12:15:57 -0300

    mais uma aula rocket seat

---
+++ Rocktseat/aula 020 - logic operators/js.js
@@ -0,0 +1,16 @@
+let email = true
+let password = false 
+let isAdm = true
+
+// and &&
+
+console.log(email && password)
+
+//or ||
+
+console.log(email || password)
+
+
+// not ! (faz o oposto)
+
+console.log(!password)
