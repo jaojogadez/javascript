c423424fc6a48b5e5152233d1334ac5a177d949d
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-16 11:33:23 -0300

    aula

---
+++ Rocktseat/aula 015 - =!/script.js
@@ -0,0 +1,28 @@
+let number = 10
+console.log(number)
+
+number++ // - isso é um incremento
+console.log(number)
+
+
+console.log(number++) // - incrementa após
+console.log(number)
+
+console.log(++number) // - incrementa antes
+
+// Decremento
+
+console.log("Decremento após: ", number--)
+console.log(number)
+console.log("Decremento antes: ", --number)
+console.log(number)
+
+
+// Incrementar mais de um (adicionar)
+number += 10 // - isso pega o contéudo e adicona a condição
+console.log(number)
+
+
+// Decrementar mais de um (remover)
+number -= 5
+console.log(number)
