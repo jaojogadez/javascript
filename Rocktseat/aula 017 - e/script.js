f06dfb13976db5e9a4725448ab254ee273255392
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-16 11:55:08 -0300

    MAIS  2 aulas

---
+++ Rocktseat/aula 017 - e/script.js
@@ -0,0 +1,15 @@
+let one = 1 
+let two = 2
+
+// igual a ==
+console.log("IGAUAL A")
+console.log(one == two)
+console.log(one == 1)
+console.log(one == "1")
+
+// diferente de !=
+
+console.log("DIFERENTE DE")
+console.log(one != two)
+
+
