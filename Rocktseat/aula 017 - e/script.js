dc54545e2576349937e6fc018af473d77147b439
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-16 11:58:38 -0300

    Update script.js

--- Rocktseat/aula 017 - e/script.js
+++ Rocktseat/aula 017 - e/script.js
@@ -13,3 +13,16 @@ console.log("DIFERENTE DE")
 console.log(one != two)
 
 
+// estritamente igual a === (tipo e valor)
+console.log("ESTRITAMENTE IGUAL A")
+console.log(one === 1)
+console.log(one === "1")
+
+// estritamente diferente de !== (tipo e valor)
+console.log("ESTRITAMENTE DIFERENTE DE")
+console.log(one !== two)
+console.log(one !== 1)
+console.log(one !== 2)
+console.log(one !== "2")
+console.log(one !== "1")
+
