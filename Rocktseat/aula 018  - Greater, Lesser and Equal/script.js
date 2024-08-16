9b366a6a0b18b7c088a9a422c1960b80af06c757
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-16 12:02:56 -0300

    update

---
+++ Rocktseat/aula 018  - Greater, Lesser and Equal/script.js
@@ -0,0 +1,12 @@
+let balance = 150
+let payment = 200
+
+// >  maior
+console.log(balance < payment)
+
+// < menor
+console.log(balance > payment)
+
+// >= maior ou igual a
+balance = 200
+console.log(balance >= payment)
