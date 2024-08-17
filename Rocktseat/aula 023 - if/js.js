9c294b4d8cc433dbd393c246eee173bf3540be94
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-17 10:13:15 -0300

    Update js.js

--- Rocktseat/aula 023 - if/js.js
+++ Rocktseat/aula 023 - if/js.js
@@ -1,7 +1,48 @@
 // IF
 
-let hour = 11
+let hour = 20
 
-if( hour <= 12 ){
+// if( hour <= 12 ){
+//     console.log("Bom dia")
+// }
+
+
+
+if( hour <= 12 )
     console.log("Bom dia")
+
+
+
+// ELSE
+
+let age = 19
+
+if(age < 18){
+    console.log("Você não pode dirigirar")
+}
+else{
+    console.log("Você pode dirigir")
+}
+
+
+// IF ELSE IF
+
+if(hour <= 12){
+    console.log("Bomdia")
+}
+else if(hour > 18){
+    console.log("Boa noite")
+}
+else{
+    console.log("Boa tarde")
+}
+
+if(hour <= 12){
+    console.log("Bomdia")
+}
+else if(hour > 12 && hour <= 18){
+    console.log("Boa tarde")
+}
+else{
+    console.log("Boa noite")
 }
