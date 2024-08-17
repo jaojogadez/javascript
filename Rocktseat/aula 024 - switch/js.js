080bd23b0e8f66011dcbea388a3177b0e3e3e33f
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-17 10:20:24 -0300

    mais uma aula

---
+++ Rocktseat/aula 024 - switch/js.js
@@ -0,0 +1,15 @@
+let option = 2
+
+switch (option){
+    case 1:
+        console.log("Consultar pedido")
+       break
+    case 2:
+        console.log("Falar com a atendente")
+       break
+    case 3:
+        console.log("Cancelar pedido")
+       break
+    default:
+        console.log("Inválido")
+}
