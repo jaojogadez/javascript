f88cae7228597dac986767941330ca9297a70a59
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-21 12:19:12 -0300

    docjs comentent

---
+++ Rocktseat/aula 30 - Doc Coment/script.js
@@ -0,0 +1,23 @@
+/*
+    Comentário de documentação em js (sintaxe de JSDoc.)
+    O JSDoc é um padrão para incorporar documentação no código-fonte a partir desses comentários.
+
+    Vc pode add explicações a suas funções para q outras pessoas possam utiliza-la
+*/
+
+/**
+ * Authenticates the user
+ * @param {String} email user email.
+ * @param {String} password more than 6 caracters.
+ * @returns {Number} user id.
+ */
+function signIn(email, password){
+    // Fluxo de autentificação do usuário
+
+
+
+    return 7
+}
+
+signIn("joaoheloiseneto@gmail.com", 13232343)
+
