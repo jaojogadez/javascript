a5f94f05e96baab4e2ac42986feb1532e586b8ed
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-21 12:10:02 -0300

    Update script.js

--- Rocktseat/aula 029 - Function Scope/script.js
+++ Rocktseat/aula 029 - Function Scope/script.js
@@ -3,4 +3,12 @@ showMsg("Olá Neto e Heloise")
 
 function showMsg(message){
     console.log(message)
+    endline()
+    function endline(){
+        console.log("----------------------------------")
+    }
 }
+
+showMsg("Olá tudo bem")
+// não exite nesse scope ai da erro
+// endline() 
