f4d0cd91d7413b213a3196cd8bce310f3173833f
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-22 11:49:57 -0300

    aulas

--- Rocktseat/aula 031 - Anoynimous function/script.js
+++ Rocktseat/aula 031 - Anoynimous function/script.js
@@ -1,3 +1,12 @@
-const showMenssage = function(){
-    return "Olá"
+// Função anônima(não possui nome)
+const showMenssage1 = function(){
+    return "Olá, João"
 }
+
+console.log(showMenssage1())
+
+const showMenssage2 = function(message, name){
+    return message + name
+}
+
+console.log(showMenssage2("Olá"," Thiago"))
---
+++ Rocktseat/aula 32 - Arrow Function/script.js
@@ -0,0 +1,14 @@
+// Arrow Function (pq parece uma seta)
+
+const showMessage1 = () =>{
+    console.log("Hello")
+}
+
+console.log(showMessage1)
+showMessage1()
+
+const showMessage2 = (name, email) =>{
+    console.log("Hello, ", name, "Seu email é ", email)
+}
+
+showMessage2("Cleliana.", "clelianacristina@gmail.com")
