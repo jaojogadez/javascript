dc158ae5c8e782577cc7976a7467d96afece6ac9
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-23 11:43:50 -0300

    class

---
+++ Rocktseat/aula 36 - content manipulator/scripts.js
@@ -0,0 +1,18 @@
+const guest = document.querySelector("#guest-1");
+// retorna o conteúdo como texto
+// console.log(guest.textContent);
+
+// guest.textContent = "Heloise Buteca";
+
+
+// const guest2 = document.querySelector("#guest-2 span");
+// console.log(guest2.textContent);
+// guest2.innerText = "João Proplayer"
+
+
+// console.log(guest, guest2)
+
+console.log(guest.textContent); // return content visible and ocult
+
+console.log(guest.innerText); // return only visible
+console.log(guest.innerHTML); // return HTML
