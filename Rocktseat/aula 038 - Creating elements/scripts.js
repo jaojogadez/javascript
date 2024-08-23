f93404621dcbc8a52c796da605c3e35400e3962e
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-08-23 12:17:39 -0300

    aprendi criar elementos

---
+++ Rocktseat/aula 038 - Creating elements/scripts.js
@@ -0,0 +1,28 @@
+// criando elementos com js :D
+// pega a lista
+
+
+
+function addGuest(){
+    const guests = document.querySelector("ul")
+
+    // variavel para criar a new element
+    const newGuest = document.createElement("li")
+    newGuest.classList.add('guest');
+    const newName = document.createElement("span")
+    newName.textContent = "Diego" // add the name
+
+    const guestName = document.createElement("span")
+    guestName.textContent = "Oliveira"
+
+    // newGuest.append(newName, guestName) // add após o último filho
+    //newGuest.prepend(guestName) // add antes do primeiro filho
+
+    // é mais simples 
+    newGuest.appendChild(guestName);
+
+
+    // console.log(newGuest);
+
+    guests.append(newGuest)
+}
---
+++ Rocktseat/aula/scripts.js
@@ -0,0 +1,15 @@
+let input = document.getElementById('name');
+console.log(input);
+// add classe
+// input.classList.add('input-error');
+// remove classe
+// input.classList.remove('input-error');
+
+// se não tiver a classe add, se tem remove
+// input.classList.toggle("input-error")
+
+const button = document.querySelector("button");
+console.log(button)
+
+// proprieties modific
+button.style.backgroundColor = "red"
