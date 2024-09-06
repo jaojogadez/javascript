b2b055aac336ef8e94a5f597ed280c872d574366
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-09-06 11:38:08 -0300

    Update scripts.js
    Chegou a hora do projeto

--- Rocktseat/aula 045 - Debugs/scripts.js
+++ Rocktseat/aula 045 - Debugs/scripts.js
@@ -1,38 +1,16 @@
-const input = document.querySelector('input')
+const input = document.querySelector("input")
 const form = document.querySelector("form")
 
-
-// input.addEventListener("input", () => {
-//     const value= input.value
-
-//     const regex = /\D+/g // Regex = padrão
-
-//     // Retorna o padrão encontrado na string
-//     // console.log(value.match(regex))
-
-//     // Testa se atende
-//     // const isValid = regex.test(value)
-//     // console.log(isValid)
-// })
-
-
-form.addEventListener("submit",(e) =>{
+form.addEventListener("submit", (e) => {
     e.preventDefault()
     const value = input.value
-    const regex = /\D+/g // Regex = padrão
-    if(regex.test(value)){
-        console.log(value)
-        alert("Padrão Encontrado")
+    const hasNumberRegex = /\d+/g // d - numbers  D - text
+
+    if(hasNumberRegex.test(value)){
+        alert("Texto Inválido. Digite novamente.")
     }
     else{
-        alert("Valor Inválido. Digite corretamente")
+        alert("Cadastro enviado!")
+        console.log(value)
     }
-
-    // const value = input.value.replace(regex, "")
-    // console.log(value)
 })
-
-
-
-
-
