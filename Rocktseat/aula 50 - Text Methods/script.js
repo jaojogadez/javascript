a7d884d4c740c2256659dd184f4b2529214fb7a4
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-09-20 20:49:16 -0300

    aula

---
+++ Rocktseat/aula 50 - Text Methods/script.js
@@ -0,0 +1,15 @@
+let message = "JoãoPedroOliveira"
+console.log(message)
+console.log(message.length)
+
+
+let password = "123456"
+if(password.length < 6 ){
+    console.log("A senha deve ter ao menos 6  digitos")
+}
+
+// Quantos dígitos tem um número
+let value = 12345
+console.log(value)
+console.log(String(value).length)
+console.log(value.toString().length)
