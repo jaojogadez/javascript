31b07572d06f90efa0a22b473ecfb386feb02ab7
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-09-21 18:05:35 -0300

    aula

---
+++ Rocktseat/aula 51 - Replacing and slicing text/script.js
@@ -0,0 +1,19 @@
+let message = "Estou aprendendo os fundamentos do JavaScript"
+
+// Substituindo parte de um texto
+// console.log(message.replace("os fundamento do JavaScript","Métodos de Texto"))
+
+// // Original
+// console.log(message)
+
+
+
+
+
+// Extraindo uma parte da string (start, end)
+console.log(message.slice(6, 30))
+
+// Extraindo uma parte da string de trás para frente.
+console.log(message.slice(-11))
+
+let =
