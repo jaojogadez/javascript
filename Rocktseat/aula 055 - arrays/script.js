9f385044defae36cb8a4ef84d0c4ba9712669ccd
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-12-14 11:46:15 -0300

    class arrays

---
+++ Rocktseat/aula 055 - arrays/script.js
@@ -0,0 +1,9 @@
+// Create Array
+const newArray = new Array()
+console.log(newArray)
+console.log(newArray.length)
+
+
+const avaible = new Array(10)
+console.log(avaible)
+console.log(avaible.length)
---
+++ Rocktseat/aula 056 - acessing arrays/script.js
@@ -0,0 +1,6 @@
+let fruits = ["Apple", "Banana", "Orange"]
+console.log(fruits)
+console.log(fruits.length)
+console.log(fruits[0])
+console.log(fruits[9])
+console.log(fruits[2])
