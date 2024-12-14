79ed6a951f332ea6765f1682d35f742b13480609
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-12-14 12:42:40 -0300

    Update script.js

--- Rocktseat/aula 055 - arrays/script.js
+++ Rocktseat/aula 055 - arrays/script.js
@@ -7,3 +7,8 @@ console.log(newArray.length)
 const avaible = new Array(10)
 console.log(avaible)
 console.log(avaible.length)
+
+
+const array = [1, 2, 3, 4, 5]
+array.splice(2, 1)
+console.log(array)
