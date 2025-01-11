7ed299ff3e0a2f6b40a71ccad055d15489d93cf8
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-01-10 22:00:14 -0300

    start date class

---
+++ Rocktseat/aula 066 - Data e Hora/script.js
@@ -0,0 +1,4 @@
+console.log(new Date()) // hora atual
+console.log(new Date(0)) // hora 0 do js
+
+
---
+++ Rocktseat/aula 067 - Define specify date/script.js
@@ -0,0 +1,5 @@
+// Define ano - mês - dia
+console.log(new Date(2007, 7, 6))
+
+// Define ano - mês - dia
+console.log(new Date(2025, 7, 6, 12, 30, 12))
