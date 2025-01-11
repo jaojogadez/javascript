f6579482fe778d89a0c770cad2a5951979101091
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-01-11 10:44:47 -0300

    js

---
+++ Rocktseat/aula 068 - Methods for work date/script.js
@@ -0,0 +1,26 @@
+let date = new Date()
+
+console.log(date)
+
+// Dia da semana de 0 a 6 (domingo é 0)
+console.log(date.getDay())
+
+// Dia do Mês 
+console.log(date.getDate())
+
+// mês
+console.log(date.getMonth() + 1)
+
+// ano
+console.log(date.getFullYear())
+
+// horas
+console.log(date.getHours())
+
+// minutes
+console.log(date.getMinutes())
+
+// seconds
+console.log(date.getSeconds())
+
+
