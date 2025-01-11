5082cda07c5be0e2d1677a7b7e2a79842f4228fd
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2025-01-11 10:30:24 -0300

    Update script.js

--- Rocktseat/aula 067 - Define specify date/script.js
+++ Rocktseat/aula 067 - Define specify date/script.js
@@ -1,5 +1,7 @@
 // Define ano - mês - dia
 console.log(new Date(2007, 7, 6))
 
-// Define ano - mês - dia
+// Define ano - mês - dia - hora - minuto - segundos
 console.log(new Date(2025, 7, 6, 12, 30, 12))
+
+console.log(new Date("July 3, 2025, 12:30:33")) // string
